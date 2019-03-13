e17c0313e09c962b86821d517b530858d563a935
Maxouuille <maxime.i@hotmail.com>
2019-03-13 15:17:35 +0100

    Google Maps

--- routes/ville.js
+++ routes/ville.js
@@ -1,11 +1,20 @@
 var express = require('express');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-	var ville = req.body.ville
 	var description = req.body.description
-//	description = description.replace(/\n/g, "<br>");
-  	res.render('ville', { ville: ville, description: description });
+	var ville = req.body.ville
+	var authKey = "433084224182595219021x1970"
+	var villeUrl = "https://geocode.xyz/"+ville+"?json=1&auth="+authKey
+	var req = new XMLHttpRequest();
+	req.open("GET", villeUrl, false);
+	req.send(null);
+	var jsonReq = JSON.parse(req.responseText)
+	var latt = jsonReq.latt
+	var longt = jsonReq.longt
+	//	description = description.replace(/\n/g, "<br>");
+  	res.render('ville', { ville: ville, description: description, longt: longt, latt, latt });
 });
 
 module.exports = router;
