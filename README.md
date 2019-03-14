f209947c1bbb0e69ce25bb67163b4e83b8baba8e
Maxouuille <maxime.i@hotmail.com>
2019-03-14 16:35:23 +0100

    Ajout de variable url pour les tests, modification du README.md

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-git clone https://github.com/Maxouuille/node-app.git
+Cloner le projet : 
 
-npm install
+  git clone https://github.com/Maxouuille/node-app.git
 
-DEBUG="node-app:server" npm start
+Installer les dépendances pour l'application :
+
+  cd node-app
+
+  npm install
+
+Lancé le serveur en local : 
+
+  DEBUG="node-app:server" npm start
+
+=== TEST ===
+
+Afin de faire des tests de l'application, ouvrir le test.js situé a la racine de notre projet, changé les variables ville et url afin quel corresponde au valeur désirer.
+
+Lance sur le terminal :
+
+  npm test
--- test.js
+++ test.js
@@ -7,25 +7,45 @@ var contenu;
 
 
 test('Test si index.hjs contient un formulaire', t => {
-	contenu = fs.readFileSync("./views/index.hjs", "UTF-8");
-	var result = t.regex(contenu, /<form .*>/gmi)
+	
+	contenu = fs.readFileSync("./views/index.hjs", "UTF-8");	
+	
 	t.regex(contenu, /<form .*>/gmi)
 	t.regex(contenu, /<\/form>/gmi)
 		t.pass()
 });
 
 test('Test si la page ville contient bien la variable ville lors de requetes POST', t => {
+	
 	var ville = 'Paris'
+	var url = "https://stormy-lake-19285.herokuapp.com/ville" 
 	var jsonDataObj = {'ville': ville};
 	var regex = new RegExp(ville, 'gmi');
+	
 	var req = new XMLHttpRequest();
-	req.open("POST", "https://stormy-lake-19285.herokuapp.com/ville", false);
-
+	req.open("POST", url, false);
 	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	req.send("ville=Paris");
+	req.send("ville="+ville);
+
 	t.regex(req.responseText, regex)
 	t.pass()
+});
 
+test('Test si la page ville n\'existe pas  lors de requetes POST', t => {
+	
+	var ville = 'Pakjlzcnaeoicn,ezris'
+	var url = "https://stormy-lake-19285.herokuapp.com/ville"
+	
+	var jsonDataObj = {'ville': ville};
+	var regex = new RegExp("Ville inconnus", 'gmi');
+	
+	var req = new XMLHttpRequest();
+	req.open("POST", url, false);
+	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	req.send("ville="+ville);
+
+	t.regex(req.responseText, regex)
+	t.pass()
 });
 
 
